de670c8c039b712b10c6d85c8c8b4e93fac968ae
kyh <wesaq11@gmail.com>
2022-06-27 17:06:15 +0900

    [#2] DisplayName & Assertion

--- src/main/java/com/example/javatest/Study.java
+++ src/main/java/com/example/javatest/Study.java
@@ -1,4 +1,22 @@
 package com.example.javatest;
 
 public class Study {
+
+    private int limit;
+    private StudyStatus status;
+
+    public Study(int limit) {
+        if(limit < 10) {
+            throw new IllegalStateException("값이 10 이하 입니다.");
+        }
+        this.limit = limit;
+    }
+
+    public StudyStatus getStatus() {
+        return status;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
 }
--- src/test/java/com/example/javatest/StudyTest.java
+++ src/test/java/com/example/javatest/StudyTest.java
@@ -2,6 +2,8 @@ package com.example.javatest;
 
 import org.junit.jupiter.api.*;
 
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /*
@@ -11,18 +13,52 @@ import static org.junit.jupiter.api.Assertions.*;
 
     XXXAll 애노테이션은 static 을 붙여야 한다.
  */
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
     @Test
+    @DisplayName("스터디 만들기 1")
+        // 우선순위가 조금 더 높다.
     void create1() {
-        Study study = new Study();
-        assertNotNull(study);
+        /*
+             테스트가 중간에 실패할 경우 이후 테스트에 대한 실행이 안된다
+             따라서 assertAll 을 사용해서 이후 테스트에 대한 검증이 그대로 가능하다.
+         */
+        Study study = new Study(10);
+        assertAll(
+                () -> assertNotNull(study),
+                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(),
+                        // 문자열에 중간에 연산이 들어갈 경우 람다식을 쓰는 것이 더 좋음.
+                        // 문자열만 사용해서 메세지를 나타낼 수 도 있다.
+                        () -> "스터디가 생성되면 DRAFT 여야 한다."),
+                () -> assertTrue(1 < 2)
+        );
+        System.out.println("스터디 만들기 1");
     }
 
     @Test
+    @DisplayName("스터디 생성시 예외 발생 테스트")
     void create2() {
-        Study study = new Study();
-        assertNotNull(study);
+
+        IllegalStateException ex = assertThrows(IllegalStateException.class, () -> new Study(9));
+        String message = ex.getMessage();
+
+        assertEquals("값이 10 이하 입니다.", message);
+
+        System.out.println("스터디 만들기 2");
+    }
+
+    @Test
+    @DisplayName("스터디 생성시 생성 시간 테스트")
+    void create3() {
+        // assertTimeoutPreemptively 로 블럭이 끝나기 전에
+        // 특정 시간이 지날때쯤 강제로 끝 낼 수 있지만, 쓰레드 를 사용하는 곳에서는 사용하지 않는게 좋다.
+        assertTimeout(Duration.ofMillis(100), () -> {
+            new Study(10);
+            // sleep을 걸면 해당 블럭이 끝날 때까지 시간을 기다리기 떄문에 실패가 나옴.
+//            Thread.sleep(300);
+        });
+        System.out.println("스터디 만들기 3");
     }
 
     @BeforeAll
@@ -43,5 +79,6 @@ class StudyTest {
     @AfterEach
     void afterEach() {
         System.out.println("after each...");
+        System.out.println();
     }
 }
